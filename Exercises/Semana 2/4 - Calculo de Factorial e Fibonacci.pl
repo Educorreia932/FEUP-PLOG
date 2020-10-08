5ba65844049fcb966a735d55b822de72c9521f76
Educorreia932 <skelozard1@gmail.com>
2020-10-08 10:37:57 +0100

    Finished exercise 4 - Calculo de Factorial e Fibonacci

---
+++ Exercises/Semana 2/4 - Calculo de Factorial e Fibonacci.pl
@@ -0,0 +1,18 @@
+/* 4. a) */
+
+factorial(0, 1).
+factorial(N, Valor) :-
+	N > 0,
+	N1 is N - 1,
+	factorial(N1, Valor1),
+	Valor is N * Valor1.
+
+/* 4. b) */
+
+fibonacci(0, 1).
+fibonacci(1, 1).
+fibonacci(N, F) :-
+    N > 1,
+    N1 is N - 1, fibonacci(N1, F1),
+    N2 is N - 2, fibonacci(N2, F2),
+    F is F1 + F2. 
