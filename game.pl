0947d42e62dfaeabb14ae801d62b9e44078a221b
Educorreia932 <skelozard1@gmail.com>
2020-10-14 16:02:59 +0100

    Started to work on the board display

--- game.pl
+++ game.pl
@@ -1,23 +1,38 @@
-play()
-display_game(+GameState, +Player)
-initial(-GameState)
-valid_moves(+GameState, +Player, -ListOfMoves).
-move(+GameState, +Move, -NewGameState).
-game_over(+GameState, -Winner).
-value(+GameState, +Player, -Value).
-choose_move(+GameState, +Player, +Level, -Move).
+% play().
+% initial(-GameState).
+% valid_moves(+GameState, +Player, -ListOfMoves).
+% move(+GameState, +Move, -NewGameState).
+% game_over(+GameState, -Winner).
+% value(+GameState, +Player, -Value).
+% choose_move(+GameState, +Player, +Level, -Move).
 
 % Initial Configuration of Board
-initialBoard([
-    [e, e, e, e, e, e],
-    [e, e, e, e, e, e],
-    [e, e, e, e, e, e],
-    [e, e, e, e, e, e],
-    [e, e, e, e, e, e],
-    [e, e, e, e, e, e]
+board([
+    [[w, 5], [], [], [], [], []],
+    [[], [], [], [], [], []],
+    [[], [], [], [], [], []],
+    [[], [], [], [], [], []],
+    [[], [], [], [], [], []],
+    [[], [], [], [], [], []]
 ]).
 
-piece(e, ' '). % Empty
-piece(b, 'B'). % Black
-piece(g, 'G'). % Green
-piece(w, 'W'). % White    
+display_row([]).
+display_row([A|B]) :-
+    write(A),
+    display_row(B).
+
+display_board([]).
+display_board([H|T]) :-
+    display_row(H), nl,
+    display_board(T).
+
+generate_board.
+
+% Convert piece type to output
+piece([], ' '). % Empty
+piece([b|T], 'B'). % Black
+piece([g|T], 'G'). % Green
+piece([w|T], 'W'). % White    
+
+display_game :-
+    write('Board').
